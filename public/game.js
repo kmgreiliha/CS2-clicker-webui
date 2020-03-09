c62001546c60d30b93f5055e70d7fdef5115ad2a
klintamadara <55625616+klintamadara@users.noreply.github.com>
2020-03-09 23:47:37 +0200

    change to localhost

--- public/game.js
+++ public/game.js
@@ -2,7 +2,7 @@
 // Change "test" to any username you'd like to start a new game
 var username = "test";
 
-var socket = io.connect("https://tictactoe.info/", {transports: ['websocket']});
+var socket = io.connect("localhost:8080", {transports: ['websocket']});
 setupSocket();
 
 
@@ -10,14 +10,41 @@ function setupSocket() {
 
     // This function is called whenever a new game state is received from the server
     socket.on('gameState', function (jsonGameState) {
-        console.log(jsonGameState);
-
-        // TODO: Display the game state on your GUI
+        //console.log(jsonGameState);
         // You must display: current gold, and the name, number owned, and cost for each type of equipment
 
+        const gameData = JSON.parse(jsonGameState);
+        const username = document.getElementById("username");
+        const gold = document.getElementById("gold");
+
+        const shovelName = document.getElementById("shovelName");
+        const shovelOwned = document.getElementById("shovelOwned");
+        const shovelCost = document.getElementById("shovelCost");
+
+        const excavatorName = document.getElementById("excavatorName");
+        const excavatorOwned = document.getElementById("excavatorOwned");
+        const excavatorCost = document.getElementById("excavatorCost");
+
+        const mineName = document.getElementById("mineName");
+        const mineOwned = document.getElementById("mineOwned");
+        const mineCost = document.getElementById("mineCost");
+
+        username.innerHTML = gameData.username;
+        gold.innerHTML = gameData.gold
+
+        shovelName.innerHTML = gameData.equipment.shovel.name
+        shovelOwned.innerHTML = gameData.equipment.shovel.numberOwned
+        shovelCost.innerHTML = gameData.equipment.shovel.cost
+
+        excavatorName.innerHTML = gameData.equipment.excavator.name
+        excavatorOwned.innerHTML = gameData.equipment.excavator.numberOwned
+        excavatorCost.innerHTML = gameData.equipment.excavator.cost
+
+        mineName.innerHTML = gameData.equipment.mine.name
+        mineOwned.innerHTML = gameData.equipment.mine.numberOwned
+        mineCost.innerHTML = gameData.equipment.mine.cost
     });
 }
-
 
 function initializeGame() {
     socket.emit("register", username);
